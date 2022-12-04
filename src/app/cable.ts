376ae7d3d23e3d3be69534a27566afd80d79e04e
Carsten Bauer <carsten.ba@t-online.de>
2022-12-04 20:39:24 +0100

    Klasse Kabel ergänzt

--- src/app/cable.ts
+++ src/app/cable.ts
@@ -1,2 +1,51 @@
 export class Cable {
+  cable = {
+    laenge : Number,
+    strom : Number,
+    spannung : Number,
+    anzahlPhasen : Number,
+    spannungsfall : {
+      prozent : Number,
+      absolut :Number
+    },
+    cosPhi : Number,
+    querschnitt : Number,
+    material : Cable.material,
+    temperatur : Number,
+    verlegeart : Cable.verlegearten
+    }
+
+
+
+
+
+}
+
+
+
+  export namespace Cable
+{
+    export enum material {
+      "Kupfer",
+      "Aluminium"
+    }
+
+    //TODO Verlegearten ergänzen
+    export const verlegearten =
+    [
+      {
+        kennung : "C",
+        bezeichnung : "Verlegung in wärmegedämmten Wänden"
+      }
+    ]
+    export const WurzelDrei = 1.73205080757;
+
+    //TODO Querschnitte ergänzen
+    export const querschnitte =
+    [
+      {
+      querschnitt : 0.5,
+      strom : 1
+      }
+    ];
 }
