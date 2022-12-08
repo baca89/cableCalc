314213f8729600a9dfef442edae18cd85b0cb130
Carsten Bauer <carsten.ba@t-online.de>
2022-12-08 16:18:36 +0100

    chh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CableCalculator';
+  spannungsfall = 0;
+  kabeldaten = new FormGroup(
+    {
+      laenge : new FormControl('',[
+        Validators.required,
+        Validators.pattern("^[0-9]*$"),
+        Validators.minLength(1),
+        Validators.min(1)
+        ]),
+      spannung : new FormControl (''),
+      strom : new FormControl('')
+    }
+  );
+
+
+
+
+  calc() : void{
+    console.log(this.kabeldaten.controls.laenge.invalid)
+    console.log(this.kabeldaten);
+}
 }
